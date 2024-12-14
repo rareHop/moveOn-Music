311e5ae758a66349f6df095d17e4ea68d9f2ffdc
Gourav.Agatsuma <gouravlodhi943@gmail.com>
2024-12-14 17:53:55 +0530

    Update footer.jsx

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -5,13 +5,12 @@ export default function Footer() {
         <footer className="py-5 backdrop-blur-3xl mt-8 px-6 md:px-20 lg:px-32">
             <div>
                 <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
-                <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" href="https://github.com/r2hu1">r2hu1</a> hosted on vercel.</p>
+                <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" href="https://github.com/rareHop">rareHop</a> hosted on vercel.</p>
             </div>
             <p className="text-muted-foreground text-sm mt-2 max-w-lg">We don't own any of the songs and images it belongs to its respective owners, built for educational purposes.</p>
             <div className="flex gap-3 mt-3">
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/r2hu1/musichub">Source Code</Link>
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://rahul.eu.org">Portfolio</Link>
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/r.rah_ul">Instagram</Link>
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/zenitsu.dev">Instagram</Link>
             </div>
         </footer>
     )
